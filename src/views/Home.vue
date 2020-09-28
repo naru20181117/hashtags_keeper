8beda6e9749ee1e1f842f99c4eeb0d6dabd47e7d
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-09-28 23:15:15 +0900

    set: make the list as components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="list">
+      <Item
+        v-for="hashtag in orderBy(hashtags,'date',-1)"
+        :key="hashtag.id"
+        :id="hashtag.id"
+        :uid="hashtag.uid"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Item from '@/components/Item.vue'
+
+import { db } from '../main'
+import Vue2Filters from 'vue2-filters'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
-  }
+    Item
+  },
+  data () {
+    return {
+      hashtags: []
+    }
+  },
+  firestore () {
+    return {
+      hashtags: db.collection('hashtags')
+    }
+  },
+  mixins: [Vue2Filters.mixin]
 }
 </script>
