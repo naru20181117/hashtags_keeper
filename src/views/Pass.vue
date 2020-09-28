58bdff7b10ec668ec6f16b4f8db89c55a9f51b70
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-09-28 22:26:24 +0900

    set: make the list as components

---
+++ src/views/Pass.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Get :field="value" />
+  </div>
+</template>
+
+<script>
+import Get from "@/componets/Get"
+
+export default {
+  components: {
+    Get
+  }
+}
+</script>
