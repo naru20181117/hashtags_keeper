2f050032139ee23476634fb144e541335bb24363
塚崎成寛 <n-tsukazaki@ruby-dev.jp>
2020-10-01 23:57:12 +0900

    set: user ui page

--- src/views/User.vue
+++ src/views/User.vue
@@ -4,25 +4,44 @@
       <div class="avatar" :style="'background-image: url('+user.photoURL+')'"></div>
       <div class="texts">
         <h1>{{ user.name }}</h1>
+        <p v-if="myHashtags.length > 1">{{myHashtags.length}} hashtags</p>
+        <p v-else>{{myHashtags.length}} hashtag</p>
       </div>
+    </div>
+    <div class="list">
+      <Item
+        v-for="hashtag in orderBy(myHashtags,'date',-1)"
+        :key="hashtag.id"
+        :id="hashtag.id"
+        :uid="hashtag.uid"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import { db } from '../main'
+import firebase from 'firebase'
+import Item from '@/components/Item'
+import Vue2Filters from 'vue2-filters'
 
 export default {
+  components: {
+    Item
+  },
   data () {
     return {
-      user: {}
+      user: {},
+      myHashtags: []
     }
   },
   firestore() {
     return {
-      user: db.collection('users').doc(this.$route.params.uid)
+      user: db.collection('users').doc(this.$route.params.uid),
+      myHashtags: db.collection('hashtags').where('uid','==',this.$route.params.uid)
     }
-  }
+  },
+  mixins: [Vue2Filters.mixin]
 }
 </script>
 
